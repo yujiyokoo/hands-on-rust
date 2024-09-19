f439a14505abfab99373c5a8c16b44879fa10920
Yuji Yokoo <yujiyokoo@gmail.com>
2024-09-20 00:16:11 +0900

    Add cellular automata architect

--- dungeon-crawler/src/map_builder/mod.rs
+++ dungeon-crawler/src/map_builder/mod.rs
@@ -1,9 +1,11 @@
 use crate::prelude::*;
 
+mod automata;
 mod empty;
 mod rooms;
-use empty::EmptyArchitect;
-use rooms::RoomsArchitect;
+// use empty::EmptyArchitect;
+use automata::CellularAutomataArchitect;
+// use rooms::RoomsArchitect;
 
 const NUM_ROOMS: usize = 20;
 
@@ -83,7 +85,7 @@ impl MapBuilder {
     }
 
     pub fn new(rng: &mut RandomNumberGenerator) -> Self {
-        let mut architect = RoomsArchitect {};
+        let mut architect = CellularAutomataArchitect {};
         architect.new(rng)
     }
 
@@ -107,6 +109,29 @@ impl MapBuilder {
                 .unwrap()
                 .0,
         )
+    }
+
+    fn spawn_monsters(&self, start: &Point, rng: &mut RandomNumberGenerator) -> Vec<Point> {
+        const NUM_MONSTERS: usize = 50;
+        let mut spawnable_tiles: Vec<Point> = self
+            .map
+            .tiles
+            .iter()
+            .enumerate()
+            .filter(|(idx, t)| {
+                **t == TileType::Floor
+                    && DistanceAlg::Pythagoras.distance2d(*start, self.map.index_to_point2d(*idx))
+                        > 10.0
+            })
+            .map(|(idx, _)| self.map.index_to_point2d(idx))
+            .collect();
+        let mut spawns = Vec::new();
+        for _ in 0..NUM_MONSTERS {
+            let target_index = rng.random_slice_index(&spawnable_tiles).unwrap();
+            spawns.push(spawnable_tiles[target_index].clone());
+            spawnable_tiles.remove(target_index);
+        }
+        spawns
     }
 }
 
